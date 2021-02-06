d8728e3ed33dc8432d2324054789510d6e942ad5
albertpiacka <albertpiacka@gmail.com>
2021-02-06 10:47:42 +0100

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,47 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'folder/Inbox',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
   {
     path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    loadChildren: () => import('./views/home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+    path: 'new-workout',
+    loadChildren: () => import('./views/new-workout/new-workout.module').then( m => m.NewWorkoutPageModule)
+  },
+  {
+    path: 'workouts',
+    loadChildren: () => import('./views/workouts/workouts.module').then( m => m.WorkoutsPageModule)
+  },
+  {
+    path: 'favorites',
+    loadChildren: () => import('./views/favorites/favorites.module').then( m => m.FavoritesPageModule)
+  },
+  {
+    path: 'archived',
+    loadChildren: () => import('./views/archived/archived.module').then( m => m.ArchivedPageModule)
+  },
+  {
+    path: 'trash',
+    loadChildren: () => import('./views/trash/trash.module').then( m => m.TrashPageModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('./views/history/history.module').then( m => m.HistoryPageModule)
+  },
+  {
+    path: 'progress',
+    loadChildren: () => import('./views/progress/progress.module').then( m => m.ProgressPageModule)
+  },
 ];
 
 @NgModule({
