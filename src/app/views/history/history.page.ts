c1834fbe6c8f3cde118b55a23ab085397eaaf5f1
albertpiacka <albertpiacka@gmail.com>
2021-02-07 10:56:26 +0100

    -

--- src/app/views/history/history.page.ts
+++ src/app/views/history/history.page.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
