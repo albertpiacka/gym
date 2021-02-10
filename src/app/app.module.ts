f2a750117a5ece133a730058fd3f9fab2b20e44b
albertpiacka <albertpiacka@gmail.com>
2021-02-10 08:50:18 +0100

    added chart modile and workout modal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+// Chart.JS
+import { ChartsModule } from 'ng2-charts';
+
 // Lodash
 import { _ } from 'lodash';
+
+// Workout modal
+import { WorkoutModalPage } from './modal/workout-modal/workout-modal.page'
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,12 +17,13 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
+  declarations: [AppComponent, WorkoutModalPage],
+  entryComponents: [WorkoutModalPage],
   imports: [
     BrowserModule, 
     IonicModule.forRoot(), 
     AppRoutingModule,
+    ChartsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
