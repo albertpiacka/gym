ca98cbb15b84101f4ae6837e8768077f1332866e
albertpiacka <albertpiacka@gmail.com>
2021-02-11 13:05:56 +0100

    added quotes

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { IntroModalPage } from '../../modal/intro-modal/intro-modal.page'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  name: string = ''
+  nameSet: boolean = false
+  selectedQuote: object = {}
+
+  quotes = [
+    {
+      text: 'The last three or four reps is what makes the muscle grow. This area of pain divides a champion from someone who is not a champion.',
+      author: ' Arnold Schwarzenegger, seven-time Mr. Olympia',
+    },
+    {
+      text: 'Success usually comes to those who are too busy to be looking for it.',
+      author: 'Henry David Thoreau, poet and philosopher',
+    },
+    {
+      text: 'All progress takes place outside the comfort zone.',
+      author: 'Michael John Bobak, digital artist',
+    },
+    {
+      text: 'The only place where success comes before work is in the dictionary.',
+      author: 'Vidal Sassoon, hairstylist and businessman',
+    },
+    {
+      text: 'The clock is ticking. Are you becoming the person you want to be?',
+      author: 'Greg Plitt, fitness model',
+    },
+    {
+      text: 'Whether you think you can, or you think you can’t, you’re right.',
+      author: 'Henry Ford, industrialist',
+    },
+    {
+      text: 'The successful warrior is the average man, with laser-like focus.',
+      author: 'Bruce Lee, actor and martial artist',
+    },
+    {
+      text: 'You must expect great things of yourself before you can do them.',
+      author: 'Michael Jordan, basketball player',
+    },
+    {
+      text: 'Action is the foundational key to all success.',
+      author: 'Pablo Picasso, visual artist',
+    }
+  ]
+
+  constructor(public modalController: ModalController) { }
 
   ngOnInit() {
+
+    this.selectedQuote = this.quotes[this.getRandomInt(this.quotes.length)]
+
+    console.log(this.selectedQuote)
+
+    if(!localStorage.getItem('name')){
+      this.presentModal()
+    } else {
+      this.nameSet = true
+      this.name = localStorage.getItem('name')
+    }
   }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: IntroModalPage,
+    });
+    return await modal.present();
+  }
+
 
 }
